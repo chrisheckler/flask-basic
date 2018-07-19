57a470baf339c651f3a9cfdab2faa693c9f9b288
Chris Heckler <hecklerchris@hotmail.com>
2018-07-19 03:24:09 -0700

    install_flask started

--- reactive/flask-basic.py
+++ reactive/flask-basic.py
@@ -1,4 +1,6 @@
 import os
+
+from pathlib import Path
 from subprocess import call
 
 from charms.reactive import (
@@ -7,7 +9,7 @@ from charms.reactive import (
     set_flag,
 )
 
-from charmhelpers.core.hookenv import log, status_set
+from charmhelpers.core.hookenv import log, status_set, unitdata
 
 from charmhelpers.core.hookenv import (
     service_stop,
@@ -18,4 +20,27 @@ from charmhelpers.core.hookenv import (
 
 @when_not('config.git-repo')
 def set_broken():
+    """Git repo needed"""
+
     status_set('broken', 'Please include git repo')
+
+
+@when_not('flask.installed')
+def flask_install():
+    """Setup virtualenv,dirs,gunicorn,flask"""
+
+    status_set('maintenance', 'Installing Flask')
+
+    # Create app dir
+    flask = Path('flask').mkdir()
+
+    # Installing pip,virtualenv,gunicorn,flask
+    call(['apt-get', 'install', 'python3-pip'])
+    call(['pip', 'install', 'virtualenv'])
+    call(['cd', 'flask', 'virtualenv', '-p', 'python3', 'venv'])
+    call(['pip', 'install', '-r', 'requirements.txt'])
+    call(['cd', 'flask', 'git', 'clone',
+          'https://github.com/chrisheckler/flask_apps.git' )
+    call(['pip', 'install', 'gunicorn'])
+    call(['pip', 'install', 'Flask'])
+
