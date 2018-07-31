e2a9f63dd819efa37d69d9bcdc823acfbaafd48e
Chris Heckler <hecklerchris@hotmail.com>
2018-07-31 01:31:33 -0700

    progress so far

--- reactive/flask-basic.py
+++ reactive/flask-basic.py
@@ -1,47 +1,67 @@
 import os
 
 from pathlib import Path
-from subprocess import call, Popen
+from subprocess import call
 
 from charms.reactive import (
     when,
     when_not,
     set_flag,
 )
-
+from charmhelpers.core.templating import render
 from charmhelpers.core import unitdata
-from charmhelpers.core.hookenv import log, status_set, config
+from charmhelpers.core.hookenv import log, status_set, config, charm_dir, open_port
 from charmhelpers.core.host import (
     service_stop,
     service_start,
     service_restart,
+    service_running,
 )
+FLASK_APP = Path('/srv/flask_app')
 
 config = config()
 
-
 @when_not('config.set.git-repo')
 def set_blocked():
-    """Git repo needed"""
+    """git repo needed"""
 
-    status_set('blocked', 'Please include git repo')
+    status_set('blocked', "please set 'git-repo' in the charm config")
 
 
 @when_not('flask.installed')
 def flask_install():
-    """Setup virtualenv,dirs,gunicorn,flask"""
+    """setup virtualenv,dirs,gunicorn,flask"""
 
-    status_set('maintenance', 'Installing Flask')
+    status_set('maintenance', 'installing flask')
 
     call(['git', 'clone', config.get('git-repo'), '/home/ubuntu/flask'])
     call(['apt-get', 'install', 'python3-pip'])
     call(['pip3', 'install', 'virtualenv'])
     call(['virtualenv','-p', '/usr/bin/python3.6', '/home/ubuntu/flask/.venv'])
-    call(['/home/ubuntu/flask/.venv/bin/pip3', 'install', 'Flask'])
+    call(['/home/ubuntu/flask/.venv/bin/pip3', 'install', 'flask'])
     call(['/home/ubuntu/flask/.venv/bin/pip3', 'install', 'gunicorn'])
 
-    log('Flask Installed and Git project cloned')
+    log('flask installed and git project cloned')
     status_set('active', 'flask installed')
     set_flag('flask.installed')
 
+
+@when_not('gunicorn.config')
+@when('flask.installed',
+      'config.set.git-repo')
+def gunicorn_run():
+    """Configure Gunicorn"""
+
+    status_set('maintenance', 'setting up gunicorn')
+
+    render('gunicorn.tmpl', '/etc/systemd/system/meflask.service', context={})
+    call(['systemctl', 'meflask'])
+    call(['systemctl', 'enable', 'meflask.service'])
+
+    service_start('meflask')
+    open_port(5000)
+
+    log('gunicorn configured')
+    status_set('active', 'gunicorn is configured')
+    set_flag('gunicorn.config')
 
